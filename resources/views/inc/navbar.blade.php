3c975f3041442e57cefa1f7ccc470f4e092e2234
Nishtha9 <Nish0349@gmail.com>
2018-05-24 20:06:48 +0530

    Create Post added

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,4 +1,5 @@
 <nav class="navbar navbar-inverse">
+  <div class="container">
     <div class="navbar-header">
         <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -22,5 +23,11 @@
                 <a  href="/posts">Blog</a>
               </li>
           </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li>
+              <a href="/posts/create">Create Post </a>
+            </li>
+          </ul>
         </div>
+      </div>
       </nav>
